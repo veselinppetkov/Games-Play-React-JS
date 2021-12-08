dde30a4a1b5eb350a15051074cbb5b0ba96874c2
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2021-12-08 16:30:50 +0200

    Update Welcome-world.js

--- src/components/Welcome-world.js
+++ src/components/Welcome-world.js
@@ -1,70 +1,70 @@
-function WelcomeWorld () {
-    return (
-        <div d="welcome-message">
-        <h2>ALL new games are</h2>
-        <h3>Only in GamesPlay</h3>
-      </div>
-      <img src="./images/four_slider_img01.png" alt="hero" />
+// function WelcomeWorld () {
+//     return (
+//         <div d="welcome-message">
+//         <h2>ALL new games are</h2>
+//         <h3>Only in GamesPlay</h3>
+//       </div>
+//       <img src="./images/four_slider_img01.png" alt="hero" />
 
-      <div id="home-page">
-        <h1>Latest Games</h1>
+//       <div id="home-page">
+//         <h1>Latest Games</h1>
 
-        <div d="game">
-          <div d="image-wrap">
-            <img src="./images/CoverFire.png" />
-          </div>
-          <h3>Cover Fire</h3>
-          <div d="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div d="data-buttons">
-            <a href="#" d="btn details-btn">
-              Details
-            </a>
-          </div>
-        </div>
-        <div d="game">
-          <div d="image-wrap">
-            <img src="./images/ZombieLang.png" />
-          </div>
-          <h3>Zombie Lang</h3>
-          <div d="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div d="data-buttons">
-            <a href="#" d="btn details-btn">
-              Details
-            </a>
-          </div>
-        </div>
-        <div d="game">
-          <div d="image-wrap">
-            <img src="./images/MineCraft.png" />
-          </div>
-          <h3>MineCraft</h3>
-          <div d="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div d="data-buttons">
-            <a href="#" d="btn details-btn">
-              Details
-            </a>
-          </div>
-        </div>
+//         <div d="game">
+//           <div d="image-wrap">
+//             <img src="./images/CoverFire.png" />
+//           </div>
+//           <h3>Cover Fire</h3>
+//           <div d="rating">
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//           </div>
+//           <div d="data-buttons">
+//             <a href="#" d="btn details-btn">
+//               Details
+//             </a>
+//           </div>
+//         </div>
+//         <div d="game">
+//           <div d="image-wrap">
+//             <img src="./images/ZombieLang.png" />
+//           </div>
+//           <h3>Zombie Lang</h3>
+//           <div d="rating">
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//           </div>
+//           <div d="data-buttons">
+//             <a href="#" d="btn details-btn">
+//               Details
+//             </a>
+//           </div>
+//         </div>
+//         <div d="game">
+//           <div d="image-wrap">
+//             <img src="./images/MineCraft.png" />
+//           </div>
+//           <h3>MineCraft</h3>
+//           <div d="rating">
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//             <span>☆</span>
+//           </div>
+//           <div d="data-buttons">
+//             <a href="#" d="btn details-btn">
+//               Details
+//             </a>
+//           </div>
+//         </div>
 
-        <p d="no-articles">No games yet</p>
-      </div>
-    )
-}
+//         <p d="no-articles">No games yet</p>
+//       </div>
+//     )
+// }
